ba62b597e4ab61edc3a490f94e5e1bb68ff17c93
Anil Kumar <anilkumar769@yahoo.com>
2021-12-29 13:52:54 -0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 
-import AddPost from './AddPost';
 import './App.css';
 import EditPost from './EditPost';
 import PostList from './PostList';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import AddPost from './AddPost';
+
 
 function App() {
   return (
     <div className="App">
       <h1>MERN STACK BLOG</h1>
-      <PostList/>
-      <AddPost/>
-      <EditPost/>
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element={<PostList/>} exact/>
+          <Route path = '/addpost' element={<AddPost/>} exact/>
+          <Route path = '/editpost' element={<EditPost/>} exact/>
+          
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   );
 }
